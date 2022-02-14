6b65a88025d46370295d1eb875e8c25bba2a96e4
Alexander Konrad Kessler <skaownz234@gmail.com>
2022-02-13 20:00:46 -0500

    improved query performance

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ with app.app_context():
 
 @app.route("/OperatorsByCompanyRegistrationId", methods=['GET'])
 def operators_by_company_registration_id():
-    operators = Operator.query.filter(Operator.companies.any(Company.id.in_(
-        [x.id for x in Company.query.filter_by(registration_id=request.args['RegistrationId']).with_entities(Company.id).all()]
-    ))).all()
+    sub = Company.query.filter_by(registration_id=request.args['RegistrationId']).with_entities(Company.id).subquery()
+    operators = Operator.query.filter(Operator.companies.any(Company.id.in_(sub))).all()
     return json.dumps([
         {"id": operator.id, "name": operator.name}
         for operator in operators
@@ -25,9 +24,8 @@ def operators_by_company_registration_id():
 
 @app.route("/CompaniesByOperatorName", methods=['GET'])
 def companies_by_operator_name():
-    companies = Company.query.filter(Company.operators.any(Operator.id.in_(
-        [x.id for x in Operator.query.filter_by(name=request.args['OperatorName']).with_entities(Operator.id).all()]
-    ))).all()
+    sub = Operator.query.filter_by(name=request.args['OperatorName']).with_entities(Operator.id).subquery()
+    companies = Company.query.filter(Company.operators.any(Operator.id.in_(sub))).all()
     return json.dumps([
         {"id": company.id, "name": company.name}
         for company in companies
@@ -36,10 +34,9 @@ def companies_by_operator_name():
 
 @app.route("/CompaniesAssociatedByOperator", methods=['GET'])
 def companies_associated_by_operator():
-    operators = Operator.query.filter(Operator.companies.any(Company.id.in_(
-        [x.id for x in Company.query.filter_by(registration_id=request.args['RegistrationId']).with_entities(Company.id).all()]
-    ))).all()
-    companies = Company.query.filter(Company.operators.any(Operator.id.in_([x.id for x in operators])))
+    sub = Company.query.filter_by(registration_id=request.args['RegistrationId']).with_entities(Company.id).subquery()
+    operators = Operator.query.filter(Operator.companies.any(Company.id.in_(sub))).with_entities(Operator.id).subquery()
+    companies = Company.query.filter(Company.operators.any(Operator.id.in_(operators)))
     return json.dumps([
         {"id": company.id, "name": company.name}
         for company in companies
